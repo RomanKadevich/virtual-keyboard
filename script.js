1beef9bd1b5ce09b58e44bb64490d20afeee6813
RomanKadevich <romankadevich@gmail.com>
2023-05-01 22:29:44 +0300

    feat: added the ability to enter and edit text, added animation of keys

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 14, 15, 29, 30, 42, 43, 55-59, 61-65 - color
 // 14, 30, 42, 43, 55 - size
 //функция создает верстку клавиатуры в блоке HTML, используя массив из ключей клавиш 
-function changeNameKeys(keys){
+function changeNameKeys(keys) {
     for (let key of keys) {
         switch (key.textContent) {
             case 'Delete':
@@ -51,49 +51,97 @@ function creatRusKeyboard(arrOfKeys, htmlFieldElement) {
 
         if ((i === 14) || (i === 28) || ((i >= 54) && (i <= 58)) ||
             ((i >= 60) && (i <= 64))) {
-            htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_color' id = 'key' data =" + `${i}` + ">" + arrOfKeys[i] + "</div>"
+            htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_color' data =" + `${i}` + ">" + arrOfKeys[i] + "</div>"
         } else if ((i === 13) || (i === 29) || (i === 41) || (i === 42) ||
             (i === 54) || (i === 42) || ((i >= 54) && (i <= 58)) ||
             ((i >= 60) && (i <= 64))) {
-            htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_color keyboard__key_size' id = 'key' data =" + `${i}` + ">" + arrOfKeys[i] + "</div>"
+            htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_color keyboard__key_size' data =" + `${i}` + ">" + arrOfKeys[i] + "</div>"
 
         } else if (i === 59) {
-            htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_bs-size' id = 'key' data =" + `${i}` + ">" + arrOfKeys[i] + "</div>"
+            htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_bs-size' data =" + `${i}` + ">" + arrOfKeys[i] + "</div>"
 
         } else {
-            htmlFieldElement.innerHTML += "<div class = 'keyboard__key' id = 'key' data =" + `${i}` + ">" + arrOfKeys[i] + "</div>"
+            htmlFieldElement.innerHTML += "<div class = 'keyboard__key' data =" + `${i}` + ">" + arrOfKeys[i] + "</div>"
         }
 
 
     }
-    let keys = document.querySelectorAll('#key');
+    let keys = document.querySelectorAll('.keyboard__key');
     changeNameKeys(keys)
 }
-    function creatEngKeyboard(arrOfKeys, htmlFieldElement) {
-        for (let i = 0; i < arrOfKeys.length; i++) {
-    
-            if ((i === 14) || (i === 28) || ((i >= 54) && (i <= 58)) ||
-                ((i >= 60) && (i <= 64))) {
-                htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_color' id = 'key' data =" + '0'+`${i}` + ">" + arrOfKeys[i] + "</div>"
-            } else if ((i === 13) || (i === 29) || (i === 41) || (i === 42) ||
-                (i === 54) || (i === 42) || ((i >= 54) && (i <= 58)) ||
-                ((i >= 60) && (i <= 64))) {
-                htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_color keyboard__key_size' id = 'key' data =" + '0'+`${i}` + ">" + arrOfKeys[i] + "</div>"
-    
-            } else if (i === 59) {
-                htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_bs-size' id = 'key' data =" + '0'+`${i}` + ">" + arrOfKeys[i] + "</div>"
-    
-            } else {
-                htmlFieldElement.innerHTML += "<div class = 'keyboard__key' id = 'key' data =" + '0'+`${i}` + ">" + arrOfKeys[i] + "</div>"
-            }
-    
-    
+function creatEngKeyboard(arrOfKeys, htmlFieldElement) {
+    for (let i = 0; i < arrOfKeys.length; i++) {
+
+        if ((i === 14) || (i === 28) || ((i >= 54) && (i <= 58)) ||
+            ((i >= 60) && (i <= 64))) {
+            htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_color'  data =" + '0' + `${i}` + ">" + arrOfKeys[i] + "</div>"
+        } else if ((i === 13) || (i === 29) || (i === 41) || (i === 42) ||
+            (i === 54) || (i === 42) || ((i >= 54) && (i <= 58)) ||
+            ((i >= 60) && (i <= 64))) {
+            htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_color keyboard__key_size'  data =" + '0' + `${i}` + ">" + arrOfKeys[i] + "</div>"
+
+        } else if (i === 59) {
+            htmlFieldElement.innerHTML += "<div class = 'keyboard__key keyboard__key_bs-size'  data =" + '0' + `${i}` + ">" + arrOfKeys[i] + "</div>"
+
+        } else {
+            htmlFieldElement.innerHTML += "<div class = 'keyboard__key'  data =" + '0' + `${i}` + ">" + arrOfKeys[i] + "</div>"
         }
-    
-    let keys = document.querySelectorAll('#key');
-    changeNameKeys(keys)
+
+
     }
-// создал массив event.key русскоязычной клавиатуры
+
+    let keys = document.querySelectorAll('.keyboard__key');
+    changeNameKeys(keys)
+}
+
+
+function addInputText(keys, textarea) {
+    keys.forEach(key => {
+        key.addEventListener('click', () => {
+            // Получаем символ, соответствующий нажатой клавише
+
+            const character = key.textContent;
+
+            // Добавляем символ в конец текста в textarea
+            textarea.value += character;
+        });
+
+    });
+    document.addEventListener('keydown', (event) => {
+        event.preventDefault();
+        let character = [];
+
+        // Получаем символ, соответствующий нажатой клавише
+        for (let item of pressedRusKeys) {
+
+            if ((event.key === item) && (event.key !== 'Backspace') && (event.key !== 'Delete')) {
+                character.push(event.key);
+                textarea.value += character.join('');
+            }
+
+        }
+        for (let item of pressedEngKeys) {
+            if ((event.key === item) && (event.key !== 'Backspace') && (event.key !== 'Delete')) {
+                character.push(event.key);
+                textarea.value += character.join('');
+            }
+        }
+        if (event.key === 'Backspace') {
+
+            textarea.value = textarea.value.substring(0, textarea.value.length - 1);;
+        }
+        if (event.key === 'Delete') {
+
+            textarea.value = '';
+        }
+
+
+        // Добавляем символ в конец текста в textarea
+
+
+    });
+}
+// сАоздал массив event.key русскоязычной клавиатуры
 const pressedRusKeys = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
     'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\',
     'Delete', 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter',
@@ -115,18 +163,13 @@ keyFieldRus.className = 'keyboard__keyfield'
 const keyFieldEng = document.getElementById('keyfield-eng');
 keyFieldEng.className = 'keyboard__keyfield keyboard__keyfield_disable';
 creatRusKeyboard(pressedRusKeys, keyFieldRus);
+creatEngKeyboard(pressedEngKeys, keyFieldEng);
+let keys = document.querySelectorAll(".keyboard__key");
 
-
-addEventListener('click', () => {
-    keyFieldEng.classList.toggle('keyboard__keyfield_disable');
-    keyFieldRus.classList.toggle('keyboard__keyfield_disable');
-    if (!keyFieldEng.innerHTML) {
-        creatEngKeyboard(pressedEngKeys, keyFieldEng);
-        keyFieldRus.innerHTML = '';
-    }
-    else {
-        creatRusKeyboard(pressedRusKeys, keyFieldRus);
-        keyFieldEng.innerHTML = '';
+window.addEventListener('keydown', (event) => {
+    if (event.altKey && event.shiftKey) {
+        keyFieldEng.classList.toggle('keyboard__keyfield_disable');
+        keyFieldRus.classList.toggle('keyboard__keyfield_disable');
     }
 
 })
@@ -137,35 +180,35 @@ function cancelHighlight(el) {
     el.classList.remove('keyboard__key_active');
 }
 
-const highlightRusKey = (event) =>{
+const highlightRusKey = (event) => {
     for (let item of pressedRusKeys) {
         if (event.key === item) {
             let indexOfKey = pressedRusKeys.indexOf(item);
-             highlight(document.querySelector('[data =' + '"' + `${indexOfKey}` + '"' + ']'));
+            highlight(document.querySelector('[data =' + '"' + `${indexOfKey}` + '"' + ']'));
         }
     }
 };
-const cancelHighlightRusKey = (event) =>{
+const cancelHighlightRusKey = (event) => {
     for (let item of pressedRusKeys) {
         if (event.key === item) {
             let indexOfKey = pressedRusKeys.indexOf(item);
-             cancelHighlight(document.querySelector('[data =' + '"' + `${indexOfKey}` + '"' + ']'));
+            cancelHighlight(document.querySelector('[data =' + '"' + `${indexOfKey}` + '"' + ']'));
         }
     }
 };
-const highlightEngKey = (event) =>{
+const highlightEngKey = (event) => {
     for (let item of pressedEngKeys) {
         if (event.key === item) {
             let indexOfKey = pressedEngKeys.indexOf(item);
-             highlight(document.querySelector('[data =' + '"' +'0'+`${indexOfKey}` + '"' + ']'));
+            highlight(document.querySelector('[data =' + '"' + '0' + `${indexOfKey}` + '"' + ']'));
         }
     }
 };
-const cancelHighlightEngKey = (event) =>{
+const cancelHighlightEngKey = (event) => {
     for (let item of pressedEngKeys) {
         if (event.key === item) {
             let indexOfKey = pressedEngKeys.indexOf(item);
-         cancelHighlight(document.querySelector('[data =' + '"'+'0'+ `${indexOfKey}` + '"' + ']'));
+            cancelHighlight(document.querySelector('[data =' + '"' + '0' + `${indexOfKey}` + '"' + ']'));
         }
     }
 };
@@ -176,3 +219,61 @@ document.addEventListener('keyup', cancelHighlightRusKey)
 
 document.addEventListener('keydown', highlightEngKey);
 document.addEventListener('keyup', cancelHighlightEngKey)
+
+// Получаем элементы textarea и кнопки
+
+
+addInputText(keys, textArea);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Обработка клавиш Shift, Alt и Ctrl
+// document.addEventListener("keydown", function(event) {
+//   if (event.key === "Shift" || event.key === "Alt" || event.key === "Control") {
+//     var key = document.querySelector(".key[data-key='" + event.key + "']");
+//     key.classList.add("active");
+//     event.preventDefault();
+//   }
+// });
+
+// document.addEventListener("keyup", function(event) {
+//   if (event.key === "Shift" || event.key === "Alt" || event.key === "Control") {
+//     var key = document.querySelector(".key[data-key='" + event.key + "']");
+//     key.classList.remove("active");
+//     event.preventDefault();
+//   }
+// });
+
+// // Обработка навигации по текстовой области
+// textarea.addEventListener("keydown", function(event) {
+//   if (event.key === "ArrowUp") {
+//     var cursorPos = textarea.selectionStart;
+//     var lineStart = textarea.value.lastIndexOf("\n", cursorPos - 1);
+//     var prevLineStart = textarea.value.lastIndexOf("\n", lineStart - 1);
+//     textarea.selectionStart = prevLineStart === -1 ? 0 : prevLineStart + 1;
+//     textarea.selectionEnd = lineStart;
+//     event.preventDefault();
+//   }
+//   else if (event.key === "ArrowDown") {
+//     var cursorPos = textarea.selectionStart;
+//     var lineEnd = textarea.value.indexOf("\n", cursorPos);
+//     var nextLineEnd = textarea.value.indexOf("\n", lineEnd + 1);
+//     textarea.selectionStart = lineEnd + 1;
+//     textarea.selectionEnd = nextLineEnd === -
